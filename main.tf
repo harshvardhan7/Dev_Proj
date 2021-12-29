64776234a2d1dcac4d68418a285d11e7b988bc2c
Harshvardhan7 <shahharshvardhan@yahoo.com>
2021-12-29 11:46:29 +0530

    Remove commented code

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ module "subnet" {
     env_prefix = var.env_prefix
 
 }
+
 #define security group module
 
 module "security_groups" {
@@ -34,16 +35,18 @@ module "security_groups" {
   env_prefix     = var.env_prefix
   container_port = var.container_port
 }
+
 #define Application Load Balancer module
+
 module "alb" {
   source              = "./modules/alb"
   vpc_id              = aws_vpc.projectvpc.id
   subnets             = module.subnet.public_subnets
   env_prefix          = var.env_prefix
   alb_security_groups = [module.security_groups.alb]
- // alb_tls_cert_arn    = var.tsl_certificate_arn
- //health_check_path   = var.health_check_path
 }
+
+#define ECS module
 
 module "ecs" {
   source                      = "./modules/ecs"
@@ -53,18 +56,4 @@ module "ecs" {
   security_group              = [module.security_groups.ecs_tasks]
   env_prefix                  = var.env_prefix
   ecr_repo_region             = var.region
- /*
-  container_port              = var.container_port
-  container_cpu               = var.container_cpu
-  container_memory            = var.container_memory
-  service_desired_count       = var.service_desired_count
-  container_environment = [
-    { name = "LOG_LEVEL",
-    value = "DEBUG" },
-    { name = "PORT",
-    value = var.container_port }
-  ]
-  container_secrets      = module.secrets.secrets_map
-  aws_ecr_repository_url = module.ecr.aws_ecr_repository_url
-  container_secrets_arns = module.secrets.application_secrets_arn*/
 }
--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -43,32 +43,7 @@ resource "aws_alb_listener" "http" {
         target_group_arn = aws_alb_target_group.main.arn
         type             = "forward"
     }
- /* default_action {
-    type = "redirect"
 
-    redirect {
-      port        = 443
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }*/
-}
-
-# Redirect traffic to target group
-/*
-resource "aws_alb_listener" "https" {
-    load_balancer_arn = aws_lb.main.id
-    port              = 443
-    protocol          = "HTTPS"
-
-    ssl_policy        = "ELBSecurityPolicy-2016-08"
-    certificate_arn   = var.alb_tls_cert_arn
-
-    default_action {
-        target_group_arn = aws_alb_target_group.main.id
-        type             = "forward"
-    }
-}*/
 
 output "aws_alb_target_group_arn" {
   value = aws_alb_target_group.main.arn
